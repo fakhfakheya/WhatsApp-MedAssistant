c742c52675519bdeec770082f5264651de871ed7
fakhfakheya <eya6.fakhfakh2002@gmail.com>
2025-11-11 23:15:22 +0100

    Mask sensitive password in detect_intent.py
    Replaced sensitive password with a placeholder.

--- detect_intent.py
+++ detect_intent.py
@@ -6,7 +6,7 @@ from handle_precautions import process_precaution_query
 # --- Connexion Neo4j ---
 uri = "neo4j://127.0.0.1:7687"
 user = "neo4j"
-password = "Sami.Eyya123456"
+password = "****"
 driver = GraphDatabase.driver(uri, auth=(user, password))
 
 # --- Historique des conversations ---
